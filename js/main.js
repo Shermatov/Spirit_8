d7c71962666bf23635978821e77fe74c3fb03149
Shermatov <shermatovdas@gmail.com>
2020-02-03 18:16:44 +0600

    spirit add

---
+++ js/main.js
@@ -0,0 +1,71 @@
+$('.mob_menu_button').on('click', function () {
+    $('.mob_menu').toggleClass('open_menu')
+});
+
+
+$('.list-item a').on('click', function () {
+    $('.mob_menu').removeClass('open_menu')
+});
+
+$('.mob_menu_button').on('click', function () {
+    $('.line1, #mob_menu_button').toggleClass('clck')
+});
+
+
+$('.mob_menu_button').on('click', function () {
+    $('.line3').toggleClass('clck')
+});
+
+$('.list-item a').on('click', function () {
+    $('.line1, #mob_menu_button, .line3').removeClass('clck')
+});
+
+
+$('#team_carousel').owlCarousel({
+    navigation: false,
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    loop:true,
+    autoHeight: true,
+    itemsCustom: [
+        [0, 1],
+        [450, 2],
+        [600, 2],
+        [700, 2],
+        [1000, 4],
+        [1200, 4],
+        [1400, 4],
+        [1600, 4]
+    ],
+});
+
+$('.clients_list ').owlCarousel({
+    loop:true,
+    items:5
+})
+$('#clients_carousel').owlCarousel({
+    navigation: false,
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    autoHeight: true,
+    itemsCustom: [
+        [0, 1],
+        [450, 2],
+        [600, 2],
+        [700, 2],
+        [1000, 4],
+        [1200, 5],
+        [1400, 5],
+        [1600, 5]
+    ],
+});
+
+
+$('#testimonials_carousel').owlCarousel({
+    items : 1,
+    margin:10,
+    autoHeight:true,
+    navigation: false,
+    slideSpeed: 300,
+    paginationSpeed: 400,
+});
---
+++ readme.md
@@ -0,0 +1,43 @@
+#MY SUMMARY 
+
+ # Name and surname: 
+ ### Dastan Shermatov 
+
+# Instagram: 
+### https://www.instagram.com/shermatov_ik/?hl=ru
+ 
+# Phone number: 
+ ### +996500240340
+ 
+# Summary:
+
+ ### A front-end web developer is responsible for implementing visual elements that users see and interact with in a web application. They are usually supported by back-end web developers, who are responsible for server-side application logic and integration of the work front-end developers do.
+
+ # Skills: 
+
+
+ ### The Complete Front-End Web Development Course! Get started as a front-end web developer using HTML, CSS, JavaScript, jQuery, and Bootstrap!
+
+#  Example code: 
+
+     .mobile-btn span:after { transform: translateY(10px); }
+ 
+     .show span:before { transform: translateY(0) rotate(-45deg); } .show span:after{ transform: translateY(0) rotate(45deg); }
+ 
+    /#################### HERO ##########################/ #hero { background: #222222; height: 100vh; }
+ 
+    .hero-content { text-align: center;
+    padding-top: 20%;
+    }
+
+# Experience: 
+ 
+## i can make up psd layouts.
+
+# Edication:
+ 
+### I study Front-End developer at an IT-RUN Programming Center.  
+
+# English:
+ 
+### I have English above beginners.
